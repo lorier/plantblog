3ef465e2959d1fe629bad42eac805c6711e8d5ce
Lorie Ransom <lorie@ransomdesign.com>
2017-06-09 08:48:54 -0700

    enable line breaks in image captions

--- themes/plantblogdev/src/plantblog_inc/general_setup.php
+++ themes/plantblogdev/src/plantblog_inc/general_setup.php
@@ -106,3 +106,95 @@ function pb_add_backtotop(){
 </a>';
 }
 
+
+
+add_shortcode('wp_caption', 'pb_img_caption_shortcode');
+add_shortcode('caption', 'pb_img_caption_shortcode');
+
+//Copied from core. This function enables me to add a pipe character to captions that gets turned into a line break.
+function pb_img_caption_shortcode( $attr, $content = null ) {
+	// New-style shortcode with the caption inside the shortcode with the link and image tags.
+	if ( ! isset( $attr['caption'] ) ) {
+		if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+			$content = $matches[1];
+			$attr['caption'] = trim( $matches[2] );
+		}
+	} elseif ( strpos( $attr['caption'], '<' ) !== false ) {
+		$attr['caption'] = wp_kses( $attr['caption'], 'post' );
+	}
+
+	/**
+	 * Filters the default caption shortcode output.
+	 *
+	 * If the filtered output isn't empty, it will be used instead of generating
+	 * the default caption template.
+	 *
+	 * @since 2.6.0
+	 *
+	 * @see img_caption_shortcode()
+	 *
+	 * @param string $output  The caption output. Default empty.
+	 * @param array  $attr    Attributes of the caption shortcode.
+	 * @param string $content The image element, possibly wrapped in a hyperlink.
+	 */
+	$output = apply_filters( 'img_caption_shortcode', '', $attr, $content );
+	if ( $output != '' )
+		return $output;
+
+	$atts = shortcode_atts( array(
+		'id'	  => '',
+		'align'	  => 'alignnone',
+		'width'	  => '',
+		'caption' => '',
+		'class'   => '',
+	), $attr, 'caption' );
+
+	$atts['width'] = (int) $atts['width'];
+	
+	if ( $atts['width'] < 1 || empty( $atts['caption'] ) )
+		return $content;
+
+	if ( ! empty( $atts['id'] ) )
+		$atts['id'] = 'id="' . esc_attr( sanitize_html_class( $atts['id'] ) ) . '" ';
+
+	$class = trim( 'wp-caption ' . $atts['align'] . ' ' . $atts['class'] );
+	
+	$new_caption = str_replace("|", "<br /><span class='linebreak'></span>", $attr['caption']);
+	
+	$html5 = current_theme_supports( 'html5', 'caption' );
+	// HTML5 captions never added the extra 10px to the image width
+	$width = $html5 ? $atts['width'] : ( 10 + $atts['width'] );
+
+	/**
+	 * Filters the width of an image's caption.
+	 *
+	 * By default, the caption is 10 pixels greater than the width of the image,
+	 * to prevent post content from running up against a floated image.
+	 *
+	 * @since 3.7.0
+	 *
+	 * @see img_caption_shortcode()
+	 *
+	 * @param int    $width    Width of the caption in pixels. To remove this inline style,
+	 *                         return zero.
+	 * @param array  $atts     Attributes of the caption shortcode.
+	 * @param string $content  The image element, possibly wrapped in a hyperlink.
+	 */
+	$caption_width = apply_filters( 'img_caption_shortcode_width', $width, $atts, $content );
+
+	$style = '';
+	if ( $caption_width ) {
+		$style = 'style="width: ' . (int) $caption_width . 'px" ';
+	}
+
+	if ( $html5 ) {
+		$html = '<figure ' . $atts['id'] . $style . 'class="' . esc_attr( $class ) . '">'
+		. do_shortcode( $content ) . '<figcaption class="wp-caption-text">' . $new_caption . '</figcaption></figure>';
+	} else {
+		$html = '<div ' . $atts['id'] . $style . 'class="' . esc_attr( $class ) . '">'
+		. do_shortcode( $content ) . '<p class="wp-caption-text">' . $new_caption . '</p></div>';
+	}
+
+	return $html;
+}
+
