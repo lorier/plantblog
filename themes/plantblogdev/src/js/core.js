0fd6b3ee0d00029aef6a55848dc61cba3affa062
Lorie Ransom <lorie@ransomdesign.com>
2022-12-22 17:11:42 -0800

    plant progress tweaks

--- themes/plantblogdev/src/js/core.js
+++ themes/plantblogdev/src/js/core.js
@@ -2,17 +2,21 @@
 
 ;(function($){
 	var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
+		slidesPerView: 1,
+		spaceBetween: 30,
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev"
+		},
+		effect: 'fade',	
+		fadeEffect: {
+			crossFade: true
+		}
 	});
 	
 	let filterableItems;
